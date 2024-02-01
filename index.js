a6a3dd682aaab213bb2f877ece4a09e84ccdcc4d
Blania <70106141+CryBlanka@users.noreply.github.com>
2024-02-01 22:46:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -135,7 +135,8 @@ const LOGOUT_SITES = {
   Allegro: ['GET', 'https://allegro.pl/wyloguj?origin_url=/'], // added by @intexpression
   OnetMail: ['GET', 'https://authorisation.grupaonet.pl/logout.html?state=logout&client_id=poczta.onet.pl.front.onetapi.pl'], // added by @intexpression
   InteriaMail: ['GET', 'https://poczta.interia.pl/logowanie/sso/logout'], // added by @intexpression
-  OLX: ['GET', '  https://www.olx.pl/account/logout'] // added by @intexpression
+  OLX: ['GET', '  https://www.olx.pl/account/logout'], // added by @intexpression
+  Roblox:  ['POST', 'https://auth.roblox.com/v2/logout'], // added by @cryblanka
 }
 
 /**
